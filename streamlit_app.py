1e4b2826de962083757590f6aadcb5957f6d2389
passlpj <123574157+passlpj@users.noreply.github.com>
2023-01-30 12:09:46 +0000

    Added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebarry Oatmeal')
+streamlit.text('Kale, Spincah & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
